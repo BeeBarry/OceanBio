a9a2fe6ddabe01c93f14beb16fbf5c1534f2733c
Bo-ITHS <barry.olof.namdari@iths.se>
2024-12-03 14:42:40 +0100

    Added repositories

---
+++ OceanBio.Server/DataAccess/Repositories/AnimalFactRepository.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using OceanBio.Shared.Entities;
+
+namespace OceanBio.Server.DataAccess.Repositories;
+
+public class AnimalFactRepository(AppDbContext context)
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<List<AnimalFact>> GetAllFactsAsync()
+    {
+        return await _context.AnimalFacts.ToListAsync();
+    }
+
+    public async Task<AnimalFact?> GetByIdAsync(int id)
+    {
+        return await _context.AnimalFacts.FindAsync(id);
+    }
+
+    public async Task AddAsync(AnimalFact fact)
+    {
+        await _context.AnimalFacts.AddAsync(fact);
+    }
+}
---
+++ OceanBio.Server/DataAccess/Repositories/AnimcalCategoryRepository.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using OceanBio.Shared.Entities;
+
+namespace OceanBio.Server.DataAccess.Repositories;
+
+public class AnimcalCategoryRepository(AppDbContext context)
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<List<AnimalCategory>> GetAllCategoriesAsync()
+    {
+        return await _context.AnimalCategories.ToListAsync();
+    }
+
+    public async Task<AnimalCategory?> GetByIdAsync(int id)
+    {
+        return await _context.AnimalCategories.FindAsync(id);
+    }
+
+    public async Task AddAsync(AnimalCategory category)
+    {
+        await _context.AnimalCategories.AddAsync(category);
+    }
+
+}
